6215a74dfa51217df60f89b743fa6d792a3c2982
Alex-Karmazin <karmazinalex@gmail.com>
2024-06-29 18:32:00 +0300

    fixed CORSMiddleware, added logging, added key_getter for groq

--- chat_endpoint/genetics_genie.py
+++ chat_endpoint/genetics_genie.py
@@ -2,31 +2,30 @@ import time
 from pathlib import Path
 from fastapi import FastAPI
 from just_agents.llm_session import LLMSession
-from literature.routes import hybrid_search
+from literature.routes import _hybrid_search
 from gpt.routes import longevity_gpt
 from genetics.main import rsid_lookup, gene_lookup, pathway_lookup, disease_lookup, sequencing_info
 from starlette.responses import StreamingResponse
 from dotenv import load_dotenv
-import os
+from just_agents.utils import RotateKeys
 from fastapi.middleware.cors import CORSMiddleware
+import loguru
+# import litellm
+# litellm.set_verbose=True
+log_path = Path(__file__)
+log_path = Path(log_path.parent, "logs", "genetics_genie.log")
+loguru.logger.add(log_path.absolute(), rotation="10 MB")
 
 
 load_dotenv(override=True)
 # What is the influence of different alleles in rs10937739 and what is MTOR gene?
 app = FastAPI(title="Genetics Genie API endpoint.")
-origins = [
-    "http://0.0.0.0:8088",
-    "http://localhost:8088",
-    "http://localhost:5173",
-    "http://0.0.0.0:5173",
-    "http://agingkills.eu:8088",
-    "http://agingkills.eu:5173"
-]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
@@ -38,7 +37,7 @@ async def default():
 
 @app.post("/v1/chat/completions")
 async def chat_completions(request: dict):
-    print(request)
+    loguru.logger.debug(request)
     # if request["model"].startswith("openrouter"):
     #     api_base = "https://openrouter.ai/api/v1"
     #     api_key = os.getenv('OPEN_ROUTER_KEY')
@@ -49,12 +48,13 @@ async def chat_completions(request: dict):
     api_base = None
     api_key = None
 
-    curent_llm: dict = {"model": request["model"], "api_base": api_base, "temperature": 0,
-                        "api_key": api_key}
+    curent_llm: dict = {"model": request["model"], "api_base": request.get("api_base", None), "temperature": request.get("temperature", 0)}
+    if request["model"].startswith("groq/"):
+        curent_llm["key_getter"] = RotateKeys("../groq_keys.txt")
 
     session: LLMSession = LLMSession(
         llm_options=curent_llm,
-        tools=[hybrid_search, longevity_gpt, rsid_lookup, gene_lookup, pathway_lookup, disease_lookup, sequencing_info]
+        tools=[_hybrid_search, rsid_lookup, gene_lookup, pathway_lookup, disease_lookup, sequencing_info, longevity_gpt]
     )
     with open(Path(Path(__file__).parent, "data", "system_prompt.txt")) as sys_prompt:
         session.instruct(sys_prompt.read())
