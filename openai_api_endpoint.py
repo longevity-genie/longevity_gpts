938e126a9d8feede52f48321750922e0267d8f89
Alex-Karmazin <karmazinalex@gmail.com>
2024-08-28 20:42:51 +0300

    precious3GPT tool added

--- openai_api_endpoint.py
+++ openai_api_endpoint.py
@@ -6,12 +6,15 @@ from literature.routes import _hybrid_search
 from genetics.main import rsid_lookup, gene_lookup, pathway_lookup, disease_lookup, sequencing_info
 from open_genes.tools import lifespan_change_db_query
 from clinical_trials.clinical_trails_router import _process_sql, clinical_trails_full_trial
+from precious3GPT.p3gpt_tool import get_omics_data
 from starlette.responses import StreamingResponse
 from dotenv import load_dotenv
 from just_agents.utils import RotateKeys
 from fastapi.middleware.cors import CORSMiddleware
 import loguru
 import yaml
+import litellm
+
 log_path = Path(__file__)
 log_path = Path(log_path.parent, "logs", "openai_api_endpoint.log")
 loguru.logger.add(log_path.absolute(), rotation="10 MB")
@@ -30,7 +33,7 @@ app.add_middleware(
 )
 
 TOOLS = [_hybrid_search, rsid_lookup, gene_lookup, pathway_lookup, disease_lookup, sequencing_info,
-             _process_sql, clinical_trails_full_trial, lifespan_change_db_query]
+             _process_sql, clinical_trails_full_trial, lifespan_change_db_query, get_omics_data]
 
 @app.get("/", description="Defalt message", response_model=str)
 async def default():
---
+++ precious3GPT/p3gpt_tool.py
@@ -0,0 +1,46 @@
+import requests
+
+API_URL = "https://cu2s6lgb4jew3tht.us-east-1.aws.endpoints.huggingface.cloud"
+headers = {
+    "Accept" : "application/json",
+    "Authorization": "Bearer ***REMOVED***",
+    "Content-Type": "application/json"
+}
+
+# tissue_options = ['skin', 'muscle', 'whole blood', 'epithelium', 'artery', 'fat tissue', 'brain', 'liver']
+# domain_options = ['methylation', 'proteomics', 'expression']
+
+def get_omics_data(age:float, tissue:str = 'whole body', drug:str = '', gender:str = '', domain:str = 'expression'):
+    """ This function retrieves omics data for given parameters.
+     age - age in years
+     tissue - is tissue name like: 'skin', 'muscle', 'whole blood', 'epithelium', 'artery', 'fat tissue', 'brain', 'liver', 'lung'
+     drug - drug or biological active substance name
+     gender - 'm' for male and 'f' for female
+     domain - must be one of these 'methylation', 'proteomics', 'expression' """
+    config_data = {'instruction': ['age_group2diff2age_group'],
+                   'tissue': tissue,
+                   'age': age,
+                   'cell': '',
+                   'efo': '',
+                   'datatype': domain,
+                   'drug': drug,
+                   'dose': '',
+                   'time': '',
+                   'case': '',
+                   'control': '',
+                   'dataset_type': '',
+                   'gender': gender,
+                   'species': 'human',
+                   'up': [],
+                   'down': []
+                   }
+    request_config = {"inputs": config_data, "mode": "meta2diff", "parameters": {
+        "temperature": 0.8,
+        "top_p": 0.2,
+        "top_k": 3550,
+        "n_next_tokens": 50,
+        "random_seed": 137
+    }}
+
+    response = requests.post(API_URL, headers=headers, json=request_config)
+    return str(response.json())
