699e9c428addafe46ddd76e0ffd1430077be8104
Alex-Karmazin <karmazinalex@gmail.com>
2024-10-16 15:54:24 +0300

    changes to work with new just-agents version

--- openai_api_endpoint.py
+++ openai_api_endpoint.py
@@ -1,6 +1,7 @@
 import time
 from pathlib import Path
 from fastapi import FastAPI, Request
+from just_agents.interfaces.IAgent import build_agent, IAgent
 from just_agents.llm_session import LLMSession
 from literature.routes import _hybrid_search
 from genetics.main import rsid_lookup, gene_lookup, pathway_lookup, disease_lookup, sequencing_info
@@ -9,7 +10,6 @@ from precious3GPT.p3gpt_tool import get_omics_data, get_enrichment
 from precious3GPT.routes import omics_router
 from starlette.responses import StreamingResponse
 from dotenv import load_dotenv
-from just_agents.utils import RotateKeys
 from fastapi.middleware.cors import CORSMiddleware
 from open_genes.tools import db_query
 import loguru
@@ -42,36 +42,36 @@ TOOLS = [_hybrid_search, rsid_lookup, gene_lookup, pathway_lookup, disease_looku
 def default():
     return "This is default page for Genetics Genie API endpoint."
 
-def get_options(request: dict):
-    with open("endpoint_options.yaml") as f:
-        options_llm = yaml.full_load(f).get(request["model"])
-    key_getter = options_llm.pop("key_getter", None)
-    if key_getter:
-        options_llm["key_getter"] = RotateKeys(key_getter)
-    prompt_path = options_llm.pop("system_prompt", None)
-    if prompt_path:
-        prompt_path = Path("prompts", prompt_path)
-        with open(prompt_path) as f:
-            if (len(request["messages"]) > 0) and (request["messages"][0]["role"] == "system"):
-                request["messages"][0]["content"] = f.read()
-            else:
-                request["messages"].insert(0, {"role": "system", "content": f.read()})
-    return options_llm
+# def get_options(request: dict):
+#     with open("endpoint_options.yaml") as f:
+#         options_llm = yaml.full_load(f).get(request["model"])
+#     key_getter = options_llm.pop("key_getter", None)
+#     if key_getter:
+#         options_llm["key_getter"] = RotateKeys(key_getter)
+#     prompt_path = options_llm.pop("system_prompt", None)
+#     if prompt_path:
+#         prompt_path = Path("prompts", prompt_path)
+#         with open(prompt_path) as f:
+#             if (len(request["messages"]) > 0) and (request["messages"][0]["role"] == "system"):
+#                 request["messages"][0]["content"] = f.read()
+#             else:
+#                 request["messages"].insert(0, {"role": "system", "content": f.read()})
+#     return options_llm
 
 
-def get_session(options_llm: dict) -> LLMSession:
-    tools_dict = {func.__name__: func for func in TOOLS}
-    tool_names = options_llm.pop("tools", None)
-    model_tools = None
-    if tool_names:
-        model_tools = [tools_dict[tool] for tool in tool_names]
-    session: LLMSession = LLMSession(
-        llm_options=options_llm,
-        tools=model_tools
-    )
-    return session
+# def get_session(options_llm: dict) -> LLMSession:
+#     tools_dict = {func.__name__: func for func in TOOLS}
+#     tool_names = options_llm.pop("tools", None)
+#     model_tools = None
+#     if tool_names:
+#         model_tools = [tools_dict[tool] for tool in tool_names]
+#     session: LLMSession = LLMSession(
+#         llm_options=options_llm,
+#         tools=model_tools
+#     )
+#     return session
 
-def message_cleaner(request: dict):
+def clean_messages(request: dict):
     for message in request["messages"]:
         if message["role"] == "user":
             content = message["content"]
@@ -82,19 +82,32 @@ def message_cleaner(request: dict):
                             if type(content[0].get("text", None)) is str:
                                 message["content"] = content[0]["text"]
 
+def remove_system_prompt(request: dict):
+    if request["messages"][0]["role"] == "system":
+        request["messages"] = request["messages"][1:]
+
+def get_agent(request):
+    with open("endpoint_options.yaml") as f:
+        agent_schema = yaml.full_load(f).get(request["model"])
+
+    return build_agent(agent_schema)
+
+
 @app.post("/v1/chat/completions")
 def chat_completions(request: dict):
     try:
         loguru.logger.debug(request)
-        options = get_options(request)
-        session = get_session(options)
-        message_cleaner(request)
+        # options = get_options(request)
+        # session = get_session(options)
+        agent:IAgent = get_agent(request)
+        clean_messages(request)
+        remove_system_prompt(request)
         if request["messages"]:
             if request.get("stream") and str(request.get("stream")).lower() != "false":
                 return StreamingResponse(
-                    session.stream_all(request["messages"], run_callbacks=False), media_type="application/x-ndjson"
+                    agent.stream(request["messages"]), media_type="application/x-ndjson"
                 )
-            resp_content = session.query_add_all(request["messages"], run_callbacks=False)
+            resp_content = agent.query(request["messages"])
         else:
             resp_content = "Something goes wrong, request did not contain messages!!!"
     except Exception as e:
@@ -104,12 +117,12 @@ def chat_completions(request: dict):
         "id": "1",
         "object": "chat.completion",
         "created": time.time(),
-        "model": options["model"],
+        "model": request["model"],
         "choices": [{"message": {"role": "assistant", "content": resp_content}}],
     }
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("openai_api_endpoint:app", host="0.0.0.0", port=8088, workers=10)
+    uvicorn.run("openai_api_endpoint:app", host="0.0.0.0", port=8088)
 
